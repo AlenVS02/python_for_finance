a2a2fd5b4b1a35f37d13b62f7b602e4e3469fb86
AlenVS02 <alenvaldezsiller@gmail.com>
2022-06-20 18:55:53 -0500

    Added more notes to exercise file and created a regular python file for testing.

---
+++ rand.py
@@ -0,0 +1,22 @@
+import numpy as np
+import random
+import quandl
+
+
+
+'''
+rand_float = random.random()
+
+print(rand_float)
+rand_int = random.randint(1,10)
+print(rand_int)
+rand_matrix = np.random.randint(1,6, (4,6))
+print(rand_matrix)
+
+'''
+
+from pandas_datareader import data as wb
+
+Apple = wb.DataReader('AAPL', data_source = 'yahoo', start = '1995-1-1')
+
+print(Apple)
