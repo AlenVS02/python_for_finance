c4ec9cb84fea457c50e9fe930e7eb3982e54eaaf
AlenVS02 <alenvaldezsiller@gmail.com>
2022-07-17 14:52:56 +0200

    Created functions to extract data and convert into CSV, Excel, and Json files.

--- rand.py
+++ rand.py
@@ -1,29 +1,51 @@
+from tracemalloc import start
 import numpy as np
 import random
 import quandl
-
-
-
-'''
-rand_float = random.random()
-
-print(rand_float)
-rand_int = random.randint(1,10)
-print(rand_int)
-rand_matrix = np.random.randint(1,6, (4,6))
-print(rand_matrix)
-
-'''
-'''
-
+import pandas as pd
 from pandas_datareader import data as wb
 
-Apple = wb.DataReader('AAPL', data_source = 'yahoo', start = '1995-1-1')
 
-print(Apple)
+def singStock(marker, dataSource, startDate):
+    stockInfo = wb.DataReader(marker, data_source= dataSource, start= startDate)['Adj Close']
+    return stockInfo
 
-'''
+def finDataToCSV(neededData):
+    allData = pd.DataFrame()
+    markers = neededData['markers']
+    srce = neededData['source']
+    startDate = neededData['date']
 
-my_data01 = quandl.get('FRED/GDP')
+    for i in markers:
+        allData[i] = singStock(i, srce, startDate)
 
-my_data01.to_csv(r'C:\Users\infin\OneDrive\Documentos\Python for Finance')
+    allData.tail(5).to_csv(r'C:\Users\infin\OneDrive\Documentos\Python for Finance\my_data02.csv')
+
+def finDataToExcel(neededData):
+    allData = pd.DataFrame()
+    markers = neededData['markers']
+    srce = neededData['source']
+    startDate = neededData['date']
+
+    for i in markers:
+        allData[i] = singStock(i, srce, startDate)
+
+    allData.tail(5).to_excel(r'C:\Users\infin\OneDrive\Documentos\Python for Finance\my_data02.xlsx')
+
+def finDataToJson(neededData):
+    allData = pd.DataFrame()
+    markers = neededData['markers']
+    srce = neededData['source']
+    startDate = neededData['date']
+
+    for i in markers:
+        allData[i] = singStock(i, srce, startDate)
+
+    allData.tail(5).to_json(r'C:\Users\infin\OneDrive\Documentos\Python for Finance\my_data02.json')
+
+
+neededData = {'markers': ['AAPL', 'MSFT', 'AMZN', 'GOOGL', 'GOOG', 'TSLA', 'UNH', 'JNJ', 'NVDA'], 'source': 'yahoo', 'date': '2022-1-1'}
+
+finDataToCSV(neededData)
+finDataToExcel(neededData)
+finDataToJson(neededData)
